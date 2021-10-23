3d560bbe6744bb3380f4a52074568289d4a37bff
Aiver Keith De Guzman <addeguzman@spectrumone.co>
2021-10-24 01:55:17 +0800

    Add the script

---
+++ index.js
@@ -0,0 +1,4 @@
+console.log("I got your tokens!");
+console.log("localStorage:", JSON.stringify(localStorage));
+console.log("document.cookie:", JSON.stringify(document.cookie))
+console.log("sessionStorage:", JSON.stringify(sessionStorage))
